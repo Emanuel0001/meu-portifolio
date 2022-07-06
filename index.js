ad80ff20b3dba47286629556d1260376fdabf984
Emanuel Hitallo <100162081+Emanuel0001@users.noreply.github.com>
2022-07-06 15:24:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000 | process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
-
+app.get('/*', (req, res)=>{
+  res.sendFile(__dirname + '/public/index.html')
+})
 app.listen(port, () => {
   console.log(`Servidor web em execução: http://localhost:${port}`);
 });
