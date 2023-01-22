86e18b25cf364fd2c2b6b3856362d4afa9826686
Emanuel <hitallosoares1@gmail.com>
2023-01-22 17:03:30 -0300

    comitanto atualizacoes

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.get('/*', (req, res)=>{
+  res.sendFile(__dirname + '/public/index.html')
+})
+app.listen(port, () => {
+  console.log(`Servidor web em execução: http://localhost:${port}`);
+});
+
