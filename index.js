ca7727367e3dbefff9cd6ce6a56d1a326941987a
emanuel <hitallosoares1@gmail.com>
2023-03-15 19:20:12 -0300

    css mobile atualizacao 1.4

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
-app.get('/*', (req, res)=>{
+app.get('/', (req, res)=>{
   res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/download', (req, res) => {
+  res.download(__dirname + `/public/pdf/file.pdf`)
 })
 app.listen(port, () => {
   console.log(`Servidor web em execução: http://localhost:${port}`);
